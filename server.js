92d1361c06d0d79248d6dde66834adf6fc486760
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-02-16 21:52:12 +0100

    init c

---
+++ server.js
@@ -0,0 +1,12 @@
+
+import express from 'express';
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
