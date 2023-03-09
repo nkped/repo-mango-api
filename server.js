8608c6a8f48dca075ad0d5c86e258ecd0c66e424
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-03-09 14:50:35 +0100

    database connected

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ import express from 'express';
 const app = express()
 import mongoose from 'mongoose';
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 
-const PORT = process.env.PORT || 3001
-
+//middleware
+dotenv.config()
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+//app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
-
+ //routes
 app.get("/", (req, res) => {
     res.send('Express is here!');
 })
 
 app.post('/create', (req, res) => console.log(req.body))
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+
+//server
+const port = process.env.PORT || 3001
+
+
+//db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database Connected...'));
+
+
+
+app.listen(port, () => console.log(`Server running on port: ${port}`))
