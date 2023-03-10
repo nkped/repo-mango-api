1edf01cb67b244c5122e9569368ac65a20b6c075
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-03-10 19:35:50 +0100

    close c

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ import dotenv from 'dotenv'
 
 
 //middleware
-dotenv.config()
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+dotenv.config()
+//app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
 
@@ -24,19 +24,25 @@ const Post = mongoose.model('post', postSchema)
 
 //routes
 app.get("/", (req, res) => {
-    res.send('Express is here!');
+    res.send('Express is here!')
 })
 
-app.post('/create', (req, res) => {
+app.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
         description: req.body.description
-    }).then((doc) => console.log(doc))
+    }).then((doc) => console.log(doc)).catch((err) => console.log(err))
 })
 
 
+app.get('/posts', (req, res) => {
+    Post.find()
+    .then((items) => res.json(items))
+    .catch((err) => console.log(err))
+})
+
 //server
-const port = process.env.PORT || 3001
+//const port = process.env.PORT || 3001
 
 
 //db
@@ -45,10 +51,12 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 })
 
-const db = mongoose.connection;
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log('Database Connected...'));
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Database Connected...'))
 
 
 
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+app.listen(3001, () => console.log('Server running on port 3001'))
+
+//`Server running on port: ${port}`
